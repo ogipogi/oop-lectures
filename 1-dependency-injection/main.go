1906841174828f76f53c7561453ee0176e58a961
Graziano Felline <ch33rs@gmail.com>
2024-05-23 20:43:14 +0200

    add GreetWithoutDependencyInjection as example

--- 1-dependency-injection/main.go
+++ 1-dependency-injection/main.go
@@ -6,10 +6,18 @@ import (
 	"os"
 )
 
+func GreetWithoutDependencyInjection(name string) {
+	// without Dependency Injection
+	fmt.Printf("Hello, %s", name)
+}
+
 func Greet(writer io.Writer, name string) {
+	// with Dependency Injection
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
 func main() {
+	GreetWithoutDependencyInjection("Chris")
+
 	Greet(os.Stdout, "Chris")
 }
