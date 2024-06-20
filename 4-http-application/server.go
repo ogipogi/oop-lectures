15100bcd40285d235f1abf06d3b4732df6d4ae80
Graziano Felline <ch33rs@gmail.com>
2024-06-20 19:14:50 +0200

    implement TestStoreWins

--- 4-http-application/server.go
+++ 4-http-application/server.go
@@ -8,10 +8,14 @@ import (
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-	} else if player == "Floyd" {
-		fmt.Fprint(w, "10")
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+	} else {
+		if player == "Pepper" {
+			fmt.Fprint(w, "20")
+		} else if player == "Floyd" {
+			fmt.Fprint(w, "10")
+		}
 	}
 }
 
