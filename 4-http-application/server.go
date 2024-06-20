2df683ad301a667d2b640b29dea8d8980238f0e0
Graziano Felline <ch33rs@gmail.com>
2024-06-20 19:07:26 +0200

    implement dynamic player response

--- 4-http-application/server.go
+++ 4-http-application/server.go
@@ -7,7 +7,12 @@ import (
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := r.URL.Path[len("/players/"):]
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+	} else if player == "Floyd" {
+		fmt.Fprint(w, "10")
+	}
 }
 
 func main() {
