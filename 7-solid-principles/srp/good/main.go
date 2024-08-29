574e8804df01f9fa577ce9a0bcf313fea8e79dc4
Graziano Felline <ch33rs@gmail.com>
2024-08-29 19:09:38 +0200

    implement solution to srp exercise

--- 7-solid-principles/srp/good/main.go
+++ 7-solid-principles/srp/good/main.go
@@ -25,17 +25,56 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
 type OutPrinter struct{}
 
 func (op OutPrinter) PrintArea(shape Shape) {
 	fmt.Printf("The area is: %f\n", shape.Area())
 }
 
+func (op OutPrinter) PrintAreaSum(sum float64) {
+	fmt.Printf("The sum of the areas is: %f\n", sum)
+}
+
+type SumCalculator struct {
+	shapes []Shape
+}
+
+func (sc SumCalculator) addShape(shape Shape) {
+	sc.shapes = append(sc.shapes, shape)
+}
+
+func (sc SumCalculator) Sum() float64 {
+	var sum float64
+	for _, shape := range sc.shapes {
+		sum += shape.Area()
+	}
+	return sum
+}
+
 func main() {
 	square := Square{side: 2}
 	circle := Circle{radius: 3}
-	printer := OutPrinter{}
+	rectangle := Rectangle{width: 2, height: 3}
 
+	printer := OutPrinter{}
 	printer.PrintArea(square)
 	printer.PrintArea(circle)
+	printer.PrintArea(rectangle)
+
+	sumCalculator := SumCalculator{}
+	sumCalculator.addShape(square)
+	sumCalculator.addShape(circle)
+	sumCalculator.addShape(rectangle)
+
+	sum := sumCalculator.Sum()
+	printer.PrintAreaSum(sum)
 }
